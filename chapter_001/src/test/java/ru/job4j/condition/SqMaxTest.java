74d0da8459505c3af0d6632b96ea2e965e43795d
Evgeny Chemagin <eugenechev@gmail.com>
2019-09-27 15:46:35 +0300

    4.5. Отладка программы в IDEA[#173732]

---
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SqMaxTest {
+
+    @Test
+    public void whenFirstMax() {
+        SqMax max = new SqMax();
+        int result = max.max(4, 3, 2, 1);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenSecondMax() {
+        SqMax max = new SqMax();
+        int result = max.max(3, 4, 2, 1);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenThirdMax() {
+        SqMax max = new SqMax();
+        int result = max.max(2, 3, 4, 1);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenFourthMax() {
+        SqMax max = new SqMax();
+        int result = max.max(1, 3, 2, 4);
+        assertThat(result, is(4));
+    }
+}
