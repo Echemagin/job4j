538c392723d6672d683a760ffcef4569121e85f8
Evgeny Chemagin <eugenechev@gmail.com>
2019-10-09 16:15:47 +0300

    Поправил Factorial

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -2,18 +2,9 @@ package ru.job4j.loop;
 
 public class Factorial {
     public int calc(int n) {
-        int result = 0;
-
-        if (n == 0) {
-            return 1;
-        }
+        int result = 1;
 
         for (int i = 1; i <= n; i++) {
-
-                if (result == 0) {
-                    result++;
-                }
-
                 result = result * i;
         }
 
