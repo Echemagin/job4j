8f1811248010757f5aecb0aefbe6be3237c48960
Evgeny Chemagin <eugenechev@gmail.com>
2019-10-11 17:32:17 +0300

    5.8. JavaDoc. Code convention[#173739]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,5 +1,11 @@
 package ru.job4j.calculator;
 
+/**
+ * Class Calculator для решения задачи 3.3 Элементарный калькулятор.
+ * @author Evgeny Chemagin (chevgeny@mail.ru)
+ * @since 11.10.2019
+ */
+
 public class Calculator {
 
     public static void add(double first, double second) {
@@ -22,6 +28,10 @@ public class Calculator {
         System.out.println(first + "-" + second + " = " + result);
     }
 
+    /**
+     * Метод main для запуска программы
+     * @param args - аргументы при запуске программы
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculator task.
+ *
+ * @author Evgeny Chemagin (chevgeny@mail.ru)
+ * @version 173739
+ * @since 11.10.2019
+ */
+package ru.job4j.calculator;
