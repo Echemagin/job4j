e33a0f936d1e550125b7eaf93294f61a2fdec6c5
Evgeny Chemagin <eugenechev@gmail.com>
2019-10-12 21:39:36 +0300

    Добавил javaDoc в задачу 6.0

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -1,6 +1,19 @@
 package ru.job4j.array;
 
+/**
+ * Class FindLoop для решения задачи 6.0. Заполнить массив степенями чисел.
+ * @author Evgeny Chemagin (chevgeny@mail.ru)
+ * @since 12.10.2019
+ */
+
 public class Square {
+
+    /**
+     * Метод calculate для создания массива квадратов чисел
+     * @param bound - число до которого будут производиться расчеты
+     * @return получившийся массив квадратов чисел
+     */
+
     public int[] calculate(int bound) {
         int[] rst = new int[bound];
         for (int i = 0; i < rst.length; i++) {
