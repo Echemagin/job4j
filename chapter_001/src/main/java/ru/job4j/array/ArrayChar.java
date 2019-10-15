276d33a61b821664fe80d85461612fb71516d47e
Evgeny Chemagin <eugenechev@gmail.com>
2019-10-15 17:19:40 +0300

    6.4. Слово начинается с ...[#173749]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+/**
+ * Class ArrayChar для решения задачи 6.4. Слово начинается с ...
+ * @author Evgeny Chemagin (chevgeny@mail.ru)
+ * @since 14.10.2019
+ */
+
+public class ArrayChar {
+
+    /**
+     * Метод startsWith проверяет начинается ли слово с заданной последовательности символов
+     * @param word - массив символов на которые раскладывается слово
+     * @param pref - массив символов с которым сравнивается начало слова
+     * @return результат сравнения
+     */
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i < pref.length; i++) {
+            if (pref[i] != word[i]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
