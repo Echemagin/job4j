598fbc1872dbf0c33bb58775f87a0698676a57dd
Evgeny Chemagin <eugenechev@gmail.com>
2021-12-02 20:21:42 +0300

    3. Массивы и модели. [#174892]

---
+++ chapter_002/src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,34 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book firstBook = new Book("War and peace", 587);
+        Book secondBook = new Book("Clean code", 867);
+        Book thirdBook = new Book("Fairytale", 124);
+        Book fourthBook = new Book("Economy for dummies", 52);
+        Book[] booksArray = new Book[4];
+        booksArray[0] = firstBook;
+        booksArray[1] = secondBook;
+        booksArray[2] = thirdBook;
+        booksArray[3] = fourthBook;
+        for (int index = 0; index < booksArray.length; index++) {
+            Book book = booksArray[index];
+            System.out.println("Book name is: " + book.getName() + ", its page count is: " + book.getPageCount());
+        }
+        System.out.println("Rotating first and fourth book");
+        Book tempBook = booksArray[0];
+        booksArray[0] = booksArray[3];
+        booksArray[3] = tempBook;
+        for (int index = 0; index < booksArray.length; index++) {
+            Book book = booksArray[index];
+            System.out.println("Book name is: " + book.getName() + ", its page count is: " + book.getPageCount());
+        }
+        System.out.println("Showing only books which name is \"Clean code\"");
+        for (int index = 0; index < booksArray.length; index++) {
+            Book book = booksArray[index];
+            if (book.getName().equals("Clean code")) {
+                System.out.println("Book name is: " + book.getName() + ", its page count is: " + book.getPageCount());
+            }
+        }
+    }
+}
