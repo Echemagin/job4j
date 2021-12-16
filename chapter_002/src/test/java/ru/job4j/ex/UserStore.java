651e9e50d2ada16cdcf4a5189a160fdf908ccd97
Evgeny Chemagin <eugenechev@gmail.com>
2021-12-16 23:39:58 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ chapter_002/src/test/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,35 @@
+package ru.job4j.ex;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        for (User user : users) {
+            if (user.getUsername().equals(login)) {
+                return user;
+            }
+        }
+        throw new UserNotFoundException("Can't find user with such login");
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("User is invalid");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException ui) {
+            System.out.println("User is invalid");
+        } catch (UserNotFoundException uf) {
+            System.out.println("Can't find user with such login");
+        }
+    }
+}
